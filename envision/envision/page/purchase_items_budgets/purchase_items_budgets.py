5c0759089e5f43a9a3b6fa3db18103d786f8315f
Pithiya-Nilesh <pithiyanilesh87@gmail.com>
2024-04-18 19:12:24 +0530

    add pi item

--- envision/envision/page/purchase_items_budgets/purchase_items_budgets.py
+++ envision/envision/page/purchase_items_budgets/purchase_items_budgets.py
@@ -1,5 +1,7 @@
 from erpnext.accounts.utils import get_fiscal_year
 import frappe 
+from collections import defaultdict
+
 
 final_list = []
 @frappe.whitelist()
@@ -18,6 +20,7 @@ def get_data():
     get_q3_pi_items_data()
     get_q4_pi_items_data()
     
+    print("\n\n final list", final_list)
     return final_list
     
     
@@ -29,6 +32,7 @@ def get_q1_pi_items_data():
     end_date = str(fiscal[2])
 
     budget_accounts = []
+    temp_pi_item = []
     
     for a in budget.accounts:
         budget_accounts.append(a.account)
@@ -41,10 +45,19 @@ def get_q1_pi_items_data():
         pi_details = frappe.get_doc("Purchase Invoice", pi.name)
         for item in pi_details.items:
             if item.project == budget.project and item.expense_account in budget_accounts:
-                pass
-
+                temp_pi_item.append({"item": item.item_code, "amount": item.amount})
                 
-            # pass
+    # Dictionary to store accumulated amounts for each item
+    sum_dict = defaultdict(float)
+
+    # Calculate sum for each item
+    for item in temp_pi_item:
+        sum_dict[item['item']] += item['amount']
+
+    # Convert the accumulated sums into the desired format
+    pi_list = [{"item": item, "q1_actual": amount} for item, amount in sum_dict.items()]
+    for i in pi_list:
+        final_list.append(i)
             
 def get_q2_pi_items_data():
     pass
